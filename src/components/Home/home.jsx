453c557463c9b5d289e2b83cdd1bfe41d23148a5
sumitkumar2812 <sumitkumar7355289198@gmail.com>
2026-05-14 09:45:27 +0530

    background change in all pages

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "./home.css"
 import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-
-const [searchTerm, setSearchTerm] = useState("");
 const navigate = useNavigate();
 
 
-const handleSearch = () =>{
-  navigate(`/listings?city=${searchTerm}`)
-}
+const handleSearch = () => {
+    navigate("/listings")
+};
  
 return (
 <div className='home-container'>
@@ -18,11 +16,7 @@ return (
     <div className='hero-content'>
       <h1>Find your perfect Rental Home</h1>
       <p>The best properties in your favorite cities.</p>
-
-      <div className='search-wrapper'>
-        <input type='text' placeholder='search by city (e.g. Mumbai...)' value={searchTerm} onChange={(e)=> {setSearchTerm(e.target.value); console.log(e.target.value) }} />
-      <button className='search-button' onClick={handleSearch}>Search</button>
-      </div>
+      <button className='search-button' onClick={handleSearch}>Find Your Perfect Property</button>
     </div>
   </section>
 
